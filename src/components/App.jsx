5833d0a489403487259bdf304e5712b4875653b4
ZVA-23 <zva-23@ukr.net>
2022-10-07 14:20:47 +0300

    add Statistics and Section components

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,7 @@
 import { Component } from 'react';
 import { FeedbackOptions } from './FeedbackOptions/FeedbackOptions';
+import { Statistics } from './Statistics/Statistics';
+import { Section } from './Section/Section';
 
 export class App extends Component {
   state = {
@@ -19,10 +21,21 @@ export class App extends Component {
   render() {
     return (
       <>
+        <Section title='Please leave feedback'>
         <FeedbackOptions
               options={this.state}
               onLeaveFeedback={this.onLeaveFeedback}
-            />
+        />
+        </Section>
+        <Section title='Statistics'>
+        <Statistics
+          good={this.good}
+          neutral={this.neutral}
+          bad={this.bad}
+          // total={ }
+          // positivePercentage={ }
+          ></Statistics>
+          </Section>
       </>
     )
   }
